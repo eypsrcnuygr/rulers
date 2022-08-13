20b57e380260d92fcf7541af35443067089fccf0
Eyüp Sercan UYGUR <sercanuygur@gmail.com>
2022-08-13 18:33:19 +0300

    Initialize with bare minimum configuration

---
+++ lib/rulers.rb
@@ -0,0 +1,12 @@
+require "rulers/version"
+
+module Rulers
+  class Error < StandardError; end
+  
+  class Application
+    def call(env)
+      [200, {'Content-Type' => 'text/html'},
+        ["Hello from Ruby on Rulers!"]]
+    end
+  end
+end
