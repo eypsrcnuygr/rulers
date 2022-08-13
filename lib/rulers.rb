9dae266c84b79096199abcfc004d52fefa1f0b64
Eyüp Sercan UYGUR <sercanuygur@gmail.com>
2022-08-14 01:20:42 +0300

    Add controller logic

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,12 +1,36 @@
-require "rulers/version"
+# frozen_string_literal: true
 
+require 'rulers/version'
+require 'rulers/routing'
 module Rulers
-  class Error < StandardError; end
-  
   class Application
     def call(env)
-      [200, {'Content-Type' => 'text/html'},
-        ["Hello from Ruby on Rulers!"]]
+      if env['PATH_INFO'] == '/favicon.ico'
+        return [404,
+          {'Content-Type' => 'text/html'}, []]
+      end
+      klass, act = get_controller_and_action(env)
+      controller = klass&.new(env)
+      begin
+        text = controller.send(act)
+        if controller.instance_of? HomeController
+          return [302,
+            {'Content-Type' => 'text/html'}, [text]]
+        end
+      [200, { 'Content-Type' => 'text/html' },
+       [text]]
+      rescue => exception
+        [404, { 'Content-Type' => 'text/html' },
+        ["Error: #{exception}"]]
+      end
     end
+  end
+
+  class Controller
+    def initialize(env)
+      @env = env
+    end
+
+    attr_reader :env
   end
 end
